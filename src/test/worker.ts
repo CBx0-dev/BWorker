8e2b4c43be634929509bc942f6ea85f9d57f04cc
Christoph Koschel <christoph.koschel86@gmail.com>
2026-03-25 15:56:46 +0100

    Remove test and add repository url

--- src/test/worker.ts
+++
@@ -1,6 +0,0 @@
-export async function foo(x: number): Promise<void> {
-    for (let i = 0; i < x; i++) {
-        console.log("[WORKER] Hello world");
-        await new Promise<void>(resolve => setTimeout(() => resolve(), 1000))
-    }
-}
